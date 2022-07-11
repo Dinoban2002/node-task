36a62fc7de0308be10b0a9473f43ac4fac1ae1d1
Dinoban2002 <dinoban2002@gmail.com>
2022-07-11 16:32:07 +0530

    second commit

--- Tasks/subapp.js
+++ Tasks/subapp.js
@@ -4,7 +4,7 @@ const app = fastify()
 app.register(require("./routes/authenticationRoutes"));
 app.register(require("./routes/crudOperationsRoutes"));
 
-app.listen(3000, function (err, address) {
+app.listen(3009, function (err, address) {
     if (err) {
         console.error(err)
         process.exit(1)
